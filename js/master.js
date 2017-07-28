cfbf1f1ce73d843fa9aaca79629f21198d9d5426
Night Fury <dulx96@gmail.com>
2017-07-28 12:12:15 +0700

    28/7 windows, slider + counter up

--- js/master.js
+++ js/master.js
@@ -68,3 +68,82 @@ $('a[href*="#"]')
             }
         }
     });
+
+/* active menu on click */
+$(document).ready(function () {
+
+    'use strict';
+
+
+    $('.navbar-nav li a').click(function(){
+
+        'use strict';
+
+        $('.navbar-nav li a').parent().removeClass("active");
+        $(this).parent().addClass("active");
+
+    });
+});
+
+/* highlight menu item on scroll */
+$(document).ready(function () {
+    'use strict';
+    $(window).scroll(function () {
+        'use strict';
+        $("section").each(function () {
+            'use strict';
+            var bb = $(this).attr("id");
+            var hei = $(this).outerHeight();
+            var grttop = $(this).offset().top - 70;
+
+            if($(window).scrollTop() > grttop && $(window).scrollTop()< grttop + hei){
+                $(".navbar-nav li a[href='#" + bb +"']").parent().addClass("active");
+            }
+            else {
+                $(".navbar-nav li a[href='#" + bb +"']").parent().removeClass("active");
+            }
+
+
+        });
+    });
+
+});
+
+// add auto padding to header
+$(document).ready(function() {
+    'use strict';
+    setInterval( function() {
+
+        'use strict';
+        var windowHeight = $(window).height();
+
+        var containerHeight = $(".header-container").height();
+        var padTop = windowHeight - containerHeight;
+        var header = document.querySelector(".header-container");
+        header.style.paddingTop = Math.round( padTop / 2) + 'px';
+        header.style.paddingBottom = Math.round( padTop / 2) + 'px';
+
+    }, 10);
+});
+
+// slider
+$(document).ready(function() {
+
+    $('.bxslider').bxSlider({
+        slideWidth: 292.5,
+        auto: true,
+        minSlides: 1,
+        maxSlides: 3,
+        slideMargin: 50
+    });
+
+});
+
+// counter up
+$(document).ready(function () {
+   $('.counter-num').counterUp({
+       delay: 10,
+       time: 1000
+    });
+});
+
